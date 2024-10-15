0263246c9dbaddeb12aa7a3ef672e5bc0db1875d
Praveenkumar210995 <pravin21995@gmail.com>
2024-10-15 16:00:29 +0530

    Update BatteryCheck.cs

--- BatteryCheck.cs
+++ BatteryCheck.cs
@@ -4,7 +4,12 @@ namespace ParadigmShiftCSharp
 {
     class BatteryStatusCheck
     {
-        static void ExpectTrue(bool expression)
+        private const float SocMin = 20f;
+        private const float SocMax = 80f;
+        private const float TempMin = 0f;
+        private const float TempMax = 45f;
+        BatteryChecks BatteryLimituse = new BatteryChecks();
+        public void ExpectTrue(bool expression)
         {
             if (!expression)
             {
@@ -13,13 +18,20 @@ namespace ParadigmShiftCSharp
             }
         }
 
-        static void ExpectFalse(bool expression)
+        public void ExpectFalse(bool expression)
         {
             if (expression)
             {
                 Console.WriteLine("Expected false, but got true");
                 Environment.Exit(1);
             }
+        }
+
+        public static bool BatteryIsOk(float temperature, float soc, float chargeRate)
+        {
+            return BatteryChecks.LimitCheck(temperature, TempMin, TempMax, "Temperature", true) &&
+                   BatteryChecks.LimitCheck(soc, SocMin, SocMax, "SoC", true) &&
+                   BatteryChecks.ChargeRateCheck(chargeRate);
         }
     }
 }
