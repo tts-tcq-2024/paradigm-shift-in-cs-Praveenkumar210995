64681ec51c3fd098864d9343b704d1916921977b
Praveenkumar210995 <pravin21995@gmail.com>
2024-10-15 16:01:17 +0530

    Update BatteryMonitor.cs

--- BatteryMonitor.cs
+++ BatteryMonitor.cs
@@ -6,8 +6,9 @@ namespace ParadigmShiftCSharp
     {
         static void Main()
         {
-            ExpectTrue(BatteryIsOk(25, 70, 0.7f));
-            ExpectFalse(BatteryIsOk(50, 85, 0.0f));
+            BatteryStatusCheck BatteryMonitor = new BatteryStatusCheck();
+            BatteryMonitor.ExpectTrue(BatteryStatusCheck.BatteryIsOk(25, 70, 0.7f));
+            BatteryMonitor.ExpectFalse(BatteryStatusCheck.BatteryIsOk(50, 85, 0.0f));
             Console.WriteLine("All checks passed!");
         }
     }
